9a4ece0541ddf16456249a6734c89b8f576caa07
Chloé Vieille <chloevieille@hotmail.com>
2019-10-24 10:31:39 +0200

    Initialisation modèle MVC

---
+++ src/App.java
@@ -0,0 +1,7 @@
+
+// fichier qui permet de run le projet
+public class App {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/data/AccesBaseDonnee.java
@@ -0,0 +1,9 @@
+package data;
+
+public class AccesBaseDonnee {
+
+    static final String BASEDEDONNEES_DRIVER = "com.mysql.jdbc.Driver";
+    static final String BASEDEDONNEES_URL = "jdbc:mysql://localhost/projetjavarecette";
+    static final String BASEDEDONNEES_USAGER = "root";
+    static final String BASEDEDONNEES_MOTDEPASSE ="";
+}
