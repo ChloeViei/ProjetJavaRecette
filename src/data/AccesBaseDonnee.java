73a2f3bdb7c0cdd2a05d7f40bcc6a1f16534473d
Chloé Vieille <chloevieille@hotmail.com>
2019-10-26 17:18:11 +0200

    Correction de la connexion à la BDD

--- src/data/AccesBaseDonnee.java
+++ src/data/AccesBaseDonnee.java
@@ -3,7 +3,7 @@ package data;
 public class AccesBaseDonnee {
 
     static final String BASEDEDONNEES_DRIVER = "com.mysql.jdbc.Driver";
-    static final String BASEDEDONNEES_URL = "jdbc:mysql://localhost/projetjavarecette";
+    static final String BASEDEDONNEES_URL = "jdbc:mysql://localhost/projetjavarecette?useSSL=false";
     static final String BASEDEDONNEES_USAGER = "root";
     static final String BASEDEDONNEES_MOTDEPASSE ="";
 }
