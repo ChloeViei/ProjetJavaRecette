e60ecf053e6ea649305c1e4b2a8a2d756a981186
Chloé Vieille <chloevieille@hotmail.com>
2019-10-30 18:25:16 +0100

    Premier test de navbar sur page vuelisteIngredient

--- src/view/Ingredient/VueListeIngredient.java
+++ src/view/Ingredient/VueListeIngredient.java
@@ -6,13 +6,18 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import model.Ingredient;
 
 import java.util.List;
 
 public class VueListeIngredient extends Scene {
 
+    protected BorderPane borderPane;
     protected GridPane grilleIngredient;
 
     private ControleurIngredient controleurIngredient = null;
@@ -20,15 +25,26 @@ public class VueListeIngredient extends Scene {
     private Button actionNaviguerAjouterIngredient;
 
     public VueListeIngredient() {
-        super(new GridPane(), 400, 400);
-        grilleIngredient = (GridPane) this.getRoot();
+        super(new BorderPane(), 400, 400);
+
+        // Création d'un bar de menu pour selectionner l'affichage de la fenetre
+        Menu menuIngredient = new Menu("Ingrédient");
+        Menu menuRecette = new Menu("Recette");
+        Menu menuListeCourse = new Menu("Liste course");
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(menuIngredient, menuRecette, menuListeCourse);
+        VBox vBox = new VBox(menuBar);
+
+        this.borderPane = (BorderPane) this.getRoot();
+        this.borderPane.setTop(vBox);
+
         this.actionNaviguerAjouterIngredient = new Button("Ajouter un ingrédient");
     }
 
     public void afficherListeIngredient(List<Ingredient> listeIngredients) {
-        this.grilleIngredient.getChildren().clear();
-
-        int numero = 0;
+        this.grilleIngredient = new GridPane();
+        
+        int numero = 1;
         this.grilleIngredient.add(new Label("Nom"), 0, numero);
 
         for (Ingredient ingredient : listeIngredients) {
@@ -55,6 +71,7 @@ public class VueListeIngredient extends Scene {
 
         this.grilleIngredient.add(this.actionNaviguerAjouterIngredient, 1, ++numero);
 
+        borderPane.setCenter(grilleIngredient);
     }
 
     public void setControleurIngredient(ControleurIngredient controleurIngredient) {
